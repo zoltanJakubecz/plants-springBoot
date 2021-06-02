4fba98eb037923837cb2eee2c447f53a9e310f71
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-02 20:06:29 +0200

    initial commit

---
+++ src/test/java/com/jakuza/plants/PlantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakuza.plants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
