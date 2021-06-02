4592036285df3de6bae4d0cc05ec6408f1439665
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-02 21:38:38 +0200

    add resttemplate bean

--- src/main/java/com/jakuza/plants/PlantsApplication.java
+++ src/main/java/com/jakuza/plants/PlantsApplication.java
@@ -2,12 +2,19 @@ package com.jakuza.plants;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PlantsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlantsApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
