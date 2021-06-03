81cd6e14c28e4129ea15b713ffd577a55b612c8b
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-03 20:53:26 +0200

    add status dao

---
+++ src/main/java/com/jakuza/plants/dao/ListingStatusJdbcDAO.java
@@ -0,0 +1,36 @@
+package com.jakuza.plants.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.jakuza.plants.model.ListingStatus;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ListingStatusJdbcDAO implements DAO<ListingStatus> {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	@Override
+	public List<ListingStatus> list() {
+		return null;
+	}
+
+	@Override
+	public void create(ListingStatus listingStatus) {
+	String sql =	"INSERT INTO status(id, status_name)";
+	jdbcTemplate.update(sql, listingStatus.getId(), listingStatus.getStatus_name());
+	}
+
+	@Override
+	public Optional<ListingStatus> get(UUID id) {
+		return null;
+	}
+
+
+
+}
