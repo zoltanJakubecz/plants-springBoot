d5396ab7942375c2ed3b7abc170b2c925785459f
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-03 20:52:29 +0200

    save data to db

--- src/main/java/com/jakuza/plants/controller/AppController.java
+++ src/main/java/com/jakuza/plants/controller/AppController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 
+import com.jakuza.plants.dao.ListingJdbcDAO;
 import com.jakuza.plants.model.Plant;
 import com.jakuza.plants.service.DataRetriver;
 
@@ -13,16 +14,19 @@ import org.springframework.stereotype.Component;
 public class AppController {
 
      private final DataRetriver dataRetriver;
+     private final ListingJdbcDAO listingJdbcDAO;
 
-     public AppController(DataRetriver dataRetriver){
+     public AppController(DataRetriver dataRetriver, ListingJdbcDAO listingJdbcDAO){
          this.dataRetriver = dataRetriver;
+         this.listingJdbcDAO = listingJdbcDAO;
      }
 
     @PostConstruct
     public void appController(){
         Plant[] plants = dataRetriver.getDataFromAPI();
         Arrays.stream(plants).forEach((plant) -> {
-            System.out.println(plant);
+//            System.out.println(plant);
+            listingJdbcDAO.create(plant);
         });
     }
     
