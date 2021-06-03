ffedeaa964ca44b1f9dc6528c6f4fce121a26311
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-03 20:54:25 +0200

    add status class

---
+++ src/main/java/com/jakuza/plants/model/ListingStatus.java
@@ -0,0 +1,15 @@
+package com.jakuza.plants.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ListingStatus{
+
+	private int id;
+	private String status_name;
+
+}
