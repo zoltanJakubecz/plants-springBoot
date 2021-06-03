0349d0046af17842d5c18bfda374f938b16d26d7
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-03 11:12:37 +0200

    add listin dao implementation

---
+++ src/main/java/com/jakuza/plants/dao/ListingJdbcDAO.java
@@ -0,0 +1,39 @@
+package com.jakuza.plants.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.jakuza.plants.model.Plant;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListingJdbcDAO implements DAO<Plant>{
+
+	private JdbcTemplate jdbcTemplate;
+
+	public ListingJdbcDAO(JdbcTemplate jdbcTemplate){
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public List<Plant> list(){
+		return null;
+	};
+
+	@Override
+	public void create(Plant plant){
+		String sql = "INSERT INTO listing(" + 
+				"id, title, description, inventory_item_location_id," + 
+				" listing_price, currency, quantity, listing_status," + 
+				" marketplace, upload_time, owner_email_address) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+		jdbcTemplate.update(sql,plant);
+	}
+
+	@Override
+	public Optional<Plant> get(UUID id){
+		return null;
+	}
+}
