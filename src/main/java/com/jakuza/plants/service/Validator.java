96c1829d877a849d96acbb56ae094baba4260837
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-06 18:56:52 +0200

    add location check

--- src/main/java/com/jakuza/plants/service/Validator.java
+++ src/main/java/com/jakuza/plants/service/Validator.java
@@ -1,30 +1,43 @@
 package com.jakuza.plants.service;
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jakuza.plants.dao.DAO;
+import com.jakuza.plants.model.Location;
 import com.jakuza.plants.model.dto.ListingsDTO;
 
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Validator
  */
 @Service
+@RequiredArgsConstructor
 public class Validator {
 
+    private final DAO<Location> locationDao; 
+
     public boolean isValid(Object object){
+
         ObjectMapper mapper = new ObjectMapper();
+            ListingsDTO item = mapper.convertValue(object, ListingsDTO.class);
 
         try {
-            ListingsDTO item = mapper.convertValue(object, ListingsDTO.class);
-            UUID uuid = UUID.fromString(item.getId());
-            return true;
+            UUID.fromString(item.getId());
         } catch (Exception e) {
             return false;
-            //TODO: handle exception
         }
+
+        if(item.getTitle() == null || item.getTitle().equals("")) return false;
+
+        Optional<Location> loc = locationDao.get(UUID.fromString(item.getLocation_id()));
+        if(!loc.isPresent()) return false;
+        return true;
     }
     
 }
