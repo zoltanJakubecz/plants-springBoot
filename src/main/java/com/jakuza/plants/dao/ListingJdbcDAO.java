37f119ebfe28a0b934fd74568302e692f966d9ed
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-06 11:57:31 +0200

    add lombok annotation instead of constructor

--- src/main/java/com/jakuza/plants/dao/ListingJdbcDAO.java
+++ src/main/java/com/jakuza/plants/dao/ListingJdbcDAO.java
@@ -9,14 +9,14 @@ import com.jakuza.plants.model.Listings;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class ListingJdbcDAO implements DAO<Listings>{
 
-	private JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-	public ListingJdbcDAO(JdbcTemplate jdbcTemplate){
-		this.jdbcTemplate = jdbcTemplate;
-	}
 
 	@Override
 	public List<Listings> list(){
