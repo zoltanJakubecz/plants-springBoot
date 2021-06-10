f511657f13242f7157facfb3117ddfc477cdd4f3
Zoltan Jakubecz <jakuzo@gmail.com>
2021-06-10 14:44:27 +0200

    add report monthly method

--- src/main/java/com/jakuza/plants/dao/ListingJdbcDAO.java
+++ src/main/java/com/jakuza/plants/dao/ListingJdbcDAO.java
@@ -1,9 +1,7 @@
 package com.jakuza.plants.dao;
 
-import java.util.List;
 
 import com.jakuza.plants.model.Listings;
-import com.jakuza.plants.model.dto.ReportFullDTO;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
@@ -28,16 +26,5 @@ public class ListingJdbcDAO{
 	}
 
 
-	public List<ReportFullDTO> getReport() {
-
-		String sql = "SELECT marketplace_name, COUNT(listing.id), ROUND(SUM(listing_price)::numeric, 2) as  sum_price from listing JOIN marketplace ON listing.marketplace = marketplace.id GROUP BY marketplace_name";
-		return jdbcTemplate.query(sql, (rs, rowNum) -> {
-				ReportFullDTO report = new ReportFullDTO();
-				report.setName(rs.getString("marketplace_name"));
-				report.setCount(rs.getInt("count"));
-				report.setTotalPrice(rs.getDouble("sum_price"));
-				return report;
-		});
-	}
 
 }
--- src/main/java/com/jakuza/plants/dao/ReportJdbcDAO.java
+++ src/main/java/com/jakuza/plants/dao/ReportJdbcDAO.java
@@ -2,6 +2,8 @@ package com.jakuza.plants.dao;
 
 import java.util.List;
 import com.jakuza.plants.model.dto.ReportFullDTO;
+import com.jakuza.plants.model.dto.ReportMonthlyDTO;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,8 @@ public class ReportJdbcDAO {
 	
 	public List<ReportFullDTO> getReport() {
 
-		String sql = "SELECT marketplace_name, COUNT(listing.id), ROUND(SUM(listing_price)::numeric, 2) as  sum_price from listing JOIN marketplace ON listing.marketplace = marketplace.id GROUP BY marketplace_name";
+		String sql = "SELECT marketplace_name, COUNT(listing.id), ROUND(SUM(listing_price)::numeric, 2) as  sum_price from listing JOIN marketplace ON listing.marketplace = marketplace.id GROUP BY marketplace_name;";
+
 		return jdbcTemplate.query(sql, (rs, rowNum) -> {
 				ReportFullDTO report = new ReportFullDTO();
 				report.setName(rs.getString("marketplace_name"));
@@ -25,4 +28,26 @@ public class ReportJdbcDAO {
 				return report;
 		});
 	}
+
+	public List<ReportMonthlyDTO> getReportMonthly() {
+
+		String sql = "SELECT date_trunc('month', listing.upload_time) as month, " 
+			+ "marketplace_name, " 
+			+ "COUNT(listing.id), "
+			+ "ROUND(sum(listing_price)::numeric, 2) " 
+			+ "FROM listing JOIN marketplace "
+			+ "ON listing.marketplace = marketplace.id "
+			+ "GROUP BY date_trunc('month', listing.upload_time), marketplace_name "
+			+ "ORDER BY month, marketplace_name asc;";
+
+		return jdbcTemplate.query(sql, (rs, rowNum) -> {
+			ReportMonthlyDTO report = new ReportMonthlyDTO();
+			report.setMonth(rs.getString("month"));
+			report.setName(rs.getString("marketplace_name"));
+			report.setCount(rs.getInt("count"));
+			report.setMTotalPrice(rs.getDouble("round"));
+			return report;
+		});
+	}		
+
 }
